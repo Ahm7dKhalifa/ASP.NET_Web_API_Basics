c1b70f56ad1f41aedd873751b098b33729d538b2
ahm7dkhalifa <ahm7dkhalifa@gmail.com>
2018-05-11 23:34:46 +0200

    ADD ch_02_Http_Verbs

---
+++ ch_01_Introduction/Samples/Products/Products/Controller/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Products.Models;
+namespace Products.Controller
+{
+    public class ProductsController : ApiController
+    {
+        //list of products
+        Product[] products = new Product[]
+       {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+       };
+
+        //api method for all products with get method 
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        //api method for only one product with get method
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ ch_01_Introduction/Samples/Products/Products/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Products.WebApiApplication" Language="C#" %>
