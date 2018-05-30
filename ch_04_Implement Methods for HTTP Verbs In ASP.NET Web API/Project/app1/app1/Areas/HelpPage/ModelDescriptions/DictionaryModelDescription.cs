94f7cf9a640550fdcb44439d65d42774123a97c1
ahm7dkhalifa <ahm7dkhalifa@gmail.com>
2018-05-31 01:42:30 +0200

    ADD ch_04 Implement Methods

---
+++ ch_04_Implement Methods for HTTP Verbs In ASP.NET Web API/Project/app1/app1/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace app1.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ch_04_Implement Methods for HTTP Verbs In ASP.NET Web API/Project/database  files/2_Create Employees TABLE.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Employees
+(
+	EmpID int primary key identity,
+	Name varchar(255),
+	Salary int
+);
